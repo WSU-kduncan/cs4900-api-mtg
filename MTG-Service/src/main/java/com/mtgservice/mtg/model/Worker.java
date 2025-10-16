26cf28998c259d14b7220150373922b4b1163d31
EthanWoe <woessner.8@wright.edu>
2025-10-16 15:21:52 -0400

    Updated Composite Keys

---
+++ MTG-Service/src/main/java/com/mtgservice/mtg/model/Worker.java
@@ -0,0 +1,31 @@
+package com.mtgservice.mtg.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Worker")
+public class Worker {
+    
+    @Id
+    @Column(name = "EmployeeID", columnDefinition = "int", nullable = false)
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.AUTO)
+    Integer employeeID;
+
+    @Column (name = "FirstName", columnDefinition = "VARCHAR(35)", nullable = false)
+    String firstName;
+
+    @Column (name = "LastName", columnDefinition = "VARCHAR(35)", nullable = false)
+    String lastName;
+
+    @Column (name = "Role", columnDefinition = "CHAR(3)", nullable = false)
+    String role;
+
+    @Column (name = "Email", columnDefinition = "VARCHAR(128)", nullable = false)
+    String email;
+}
